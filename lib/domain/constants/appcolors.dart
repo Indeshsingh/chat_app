b7cb9aeeaa31f6b4c25801f6227293b39ce9097d
Indesh Mahato <indeshmahato799@gmail.com>
2025-01-27 12:49:02 +0545

    some changes

---
+++ lib/domain/constants/appcolors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color scaffoldlight = Color(0XFFFFFFFF);
+  static const Color scaffolddark = Color(0XFF0F1828);
+  static const Color textlightmode = Color(0XFF0F1828);
+  static const Color textdarkmode = Color(0XFFF7F7FC);
+  static const Color buttonlightmode = Color(0XFF002DE3);
+  static const Color buttondarkmode = Color(0XFF375FFF);
+  static const Color textfieldlightmode = Color(0XFFF7F7FC);
+  static const Color textfielddarkmode = Color(0XFF152033);
+}
